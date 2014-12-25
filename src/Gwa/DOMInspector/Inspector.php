67a2442c2ae89cd6c639b112e7ef39c0f71f1d75
Timothy Groves <groves@greatwhiteark.com>
2014-12-25 07:44:55 +0000

    #3 Inspect full HTML document

--- src/Gwa/DOMInspector/Inspector.php
+++ src/Gwa/DOMInspector/Inspector.php
@@ -5,6 +5,7 @@ class Inspector extends Node
 {
     protected $_html;
     protected $_domdoc;
+    protected $_domdocnode;
     protected $_node;
 
     /**
@@ -17,5 +18,28 @@ class Inspector extends Node
         $this->_domdoc->preserveWhiteSpace = false;
         $this->_domdoc->loadHTML($html);
         $this->_node = $this->_domdoc->getElementsByTagName('body')->item(0);
+    }
+
+    /**
+     * @return string
+     */
+    public function getDoctype()
+    {
+        $doctype = $this->_domdoc->doctype;
+        return $doctype ?
+            $doctype->name :
+            null;
+    }
+
+    /**
+     * Returns a node representing the entire document.
+     * @return Node
+     */
+    public function document()
+    {
+        if (!isset($this->_domdocnode)) {
+            $this->_domdocnode = new Node($this->_domdoc->documentElement);
+        }
+        return $this->_domdocnode;
     }
 }
